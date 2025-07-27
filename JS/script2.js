5aa50fb94be9067e6394230f1a18858958d2761e
veronicwq <veronikavero13122010n@gmail.com>
2025-07-27 12:05:02 +0400

    new

---
+++ JS/script2.js
@@ -0,0 +1,17 @@
+let result = 5 + 5 + '5';
+console.log(result);           
+console.log(typeof result);    
+
+let email = "example@email.com";
+console.log(email.includes('@'));       
+console.log(email.length);             
+
+let word1 = "My";
+let word2 = "name";
+let word3 = "is";
+let fullName = `${word1} ${word2} ${word3} Viktor`;
+console.log(fullName);                  
+
+let userName = "Олександро";
+let payment = 300;
+alert(`Дякуємо, ${userName}! До сплати ${payment} гривень`);
